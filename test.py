7cecebb4d0844d7c12374d6eae25465739c203a4
montemerro <firat_erman@outlook.com>
2026-01-28 11:47:25 +0100

    mnist start

---
+++ test.py
@@ -0,0 +1,23 @@
+SIAP: sensitive industry activity affected parties
+2 type de SIAP: 
+- direct engagement connection
+- other concept
+
+-> inputs: client history pdf 
+- natural person 
+- legal entities 
+
+SCAP: sensitive commercial 
+
+
+tourne ca dans les decision trees : traduire un de ces decision tree et le traduire dans langraph 
+- commencer avec les deux exemples de decision simple: 
+- comment tester? streamlit? resultat attendue pour cet article?
+- dependance actual avec coreruler laquelle. celle via neuralkyc pour le pdf parser?
+- que fait Georges
+- me concentrer uniquement sur SIAP pour l'instant 
+
+
+
+- quel script tourner pour le moment? 
+- kyc checks, checker 
